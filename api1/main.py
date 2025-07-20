3d7ac2cd40f080be35e9453c6b2ded3ab16b6a30
Dung Machoo <dungmachoo@gmail.com>
2025-07-21 01:52:02 +0700

    update

--- api1/main.py
+++ api1/main.py
@@ -8,9 +8,9 @@ def call_api2():
     print("API1: Received request from user")
     try:
         #ของ docker
-        #response = requests.get("http://api2:8001/api2")
+        response = requests.get("http://api2:8001/api2")
         #ของ local
-        response = requests.get("http://127.0.0.1:8001/api2")
+        #response = requests.get("http://127.0.0.1:8001/api2")
         print("API1: Forwarded request to API2")
         return {"message": f"API1: Got response from API2: {response.text}"}
     except Exception as e:
